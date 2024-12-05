dac7fae4bbaf6b2a2d86387a4e49dbe65027ba84
ashishsshinde <74764695+ashishsshinde@users.noreply.github.com>
2024-12-05 17:05:54 +0530

    gitignore changes

---
+++ src/config/db.ts
@@ -0,0 +1,18 @@
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+export const databaseConfig = {
+  config: {
+    host: process.env.DATABASE_HOST,
+    user: process.env.DATABASE_USERNAME,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE_NAME,
+  },
+  sourcing: {
+    host: process.env.DATABASE_HOST,
+    user: process.env.DATABASE_USERNAME,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE_NAME,
+  }
+};
