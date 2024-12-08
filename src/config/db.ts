e5a549b546f92d6a8e5c67cacd897af230276bd6
ashishsshinde <74764695+ashishsshinde@users.noreply.github.com>
2024-12-08 22:56:13 +0530

    cred changes

--- src/config/db.ts
+++ src/config/db.ts
@@ -3,17 +3,12 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 export const databaseConfig = {
-  config: {
-    host: "13.203.44.227",
-    user: "svms_v4_dev",
-    password: "fe2w#1Dq1r@i5w4",
-    database: "test",
-    port : 3000
-    // host: "nonprod-v4-mysql-master-nv.c7su4okqw673.us-east-1.rds.amazonaws.com",
-    // user: "configuratorqauser",
-    // password: "Tg6gL225gF6hvF9V",
-    // database: "qa_vms_configurator",
-    // port : 3306
+  config: {    
+    host: "nonprod-v4-mysql-master-nv.c7su4okqw673.us-east-1.rds.amazonaws.com",
+    user: "configuratorqauser",
+    password: "Tg6gL225gF6hvF9V",
+    database: "qa_vms_configurator",
+    port : 3306
   },
   sourcing: {
     host: "nonprod-v4l-mysql-replica-master-1-nv.c7su4okqw673.us-east-1.rds.amazonaws.com",
