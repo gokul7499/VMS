b2b80cd5a96e506bccc69cc783fed89e556f32c6
sakshidighe2004 <sakshidighe2004@gmail.com>
2024-12-25 17:14:57 +0530

    change permissions case

--- src/constants/permissions.ts
+++ src/constants/permissions.ts
@@ -1,10 +1,10 @@
 // src/constants/permissions.ts
 
 export const Permissions = {
-    CREATE_CITY: 'srn:createCity',
-    DELETE_CITY: 'srn:deleteCity',
-    UPDATE_CITY: 'srn:updateCity',
-    VIEW_CITY: 'srn:viewCity',
-    CREATE_RATE_TYPE:'srn:createRateType'
+    CREATE_CITY: 'srn:create-city',
+    DELETE_CITY: 'srn:delete-city',
+    UPDATE_CITY: 'srn:update-city',
+    VIEW_CITY: 'srn:view-city',
+    CREATE_RATE_TYPE:'srn:create-rate-type'
   };
   
