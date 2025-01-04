0010f9cbc32216fc65f380672943f414b427de8b
Shishank Agrawal <shishank.agrawal@simplifyvms.com>
2025-01-04 19:08:23 +0530

    sa/checklist-fixes: fixed checklist and added listing api

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -68,6 +68,7 @@ import timesheetExpenseRuleGroupRoutes from "./timesheet-expense-rule-group.rout
 import expenseTypeRoute from "./expense-type.routes";
 import rateCardsRoutes from "./rate-card.route";
 import rateConfigurationsRoutes from "./rate-configurations.route";
+import checkListRoutes from "./checklist.routes";
 const basePrefix = "/config/v1/api";
 
 export default async function (app: FastifyInstance) {
@@ -140,4 +141,5 @@ export default async function (app: FastifyInstance) {
   app.register(expenseTypeRoute,{prefix:`${basePrefix}`})
   app.register(rateCardsRoutes,{prefix:`${basePrefix}`})
   app.register(rateConfigurationsRoutes,{prefix:`${basePrefix}`})
+  app.register(checkListRoutes,{prefix:`${basePrefix}`})
 }
