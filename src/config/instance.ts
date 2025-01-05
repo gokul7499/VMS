806f243b963a5cfb90303fff82f91222ae317f6b
Nikhil <nikhil.phapale@simplifyvms.com>
2025-01-05 13:46:05 +0530

    Added alter param  for db

--- src/config/instance.ts
+++ src/config/instance.ts
@@ -20,7 +20,7 @@ const initializeSequelize = async () => {
  
 const checkDatabaseConnection = async () => {
   try {
-    await  sequelize.sync();
+    await  sequelize.sync({alter: true});
     await sequelize.authenticate();
     console.log('Database connected successfully');
     return { connected: true, message: 'Database connected successfully' };
