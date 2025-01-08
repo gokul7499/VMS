6c0c8b4a29a28f1481eebb6b828863daaeec1ff5
Shishank Agrawal <shishank.agrawal@simplifyvms.com>
2025-01-08 12:34:30 +0530

    sa/checklist-rework: removed redundant picklist routes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -145,6 +145,5 @@ export default async function (app: FastifyInstance) {
   app.register(rateConfigurationsRoutes,{prefix:`${basePrefix}`})
   app.register(jobTemplate, { prefix: `${basePrefix}` });
   app.register(JobCategoryRoutes, { prefix: `${basePrefix}` });
-  app.register(picklistRoutes,{prefix:`${basePrefix}`});
   app.register(checklistRoutes,{prefix:`${basePrefix}`})
 }
