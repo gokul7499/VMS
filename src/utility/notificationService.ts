765d30859961196e40374db7c7e578c042cbbf24
chandrashekaharsadafal777 <chandrashekharsadafal777@gmail.com>
2025-01-13 11:15:31 +0530

    worked in notification service

--- src/utility/notificationService.ts
+++ src/utility/notificationService.ts
@@ -4,153 +4,126 @@ import { sequelize } from '../config/instance';
 import { NotificationDataPayload } from '../interfaces/noifications-data-payload.interface';
 
 function getNotificationUrl(): string | undefined {
-  return process.env.NOTIFICATION_URL;
+    return process.env.NOTIFICATION_URL || 'https://v4-dev.simplifysandbox.net/notification';
 }
 
 function validateToken(token: string | undefined): boolean {
-  if (!token) {
-    console.error('Notification token is not defined.');
-    return false;
-  }
-  return true;
+    if (!token) {
+        console.error('Notification token is not defined.');
+        return false;
+    }
+    return true;
 }
 
 export async function sendNotification(payload: NotificationDataPayload): Promise<void> {
-  const programData = await sequelize.query(
-    `SELECT * FROM programs WHERE id =:program_id`,
-    {
-      replacements: { program_id: payload.program_id },
-      type: QueryTypes.SELECT
-    }
-  )
-  const program = programData[0] as any
-  if (!program) {
-    console.error("Program not found for ID:", payload.program_id);
-    return;
-  }
-  const tenent_id = program.client_id || program.msp_id;
-  if (!tenent_id) {
-    console.error("Tenant ID is missing in the payload.");
-    return;
-  }
-
-  const result = await sequelize.query(
-    `SELECT logo FROM Tenant WHERE id = :tenent_id`,
-    {
-      replacements: { tenent_id },
-      type: QueryTypes.SELECT
-    }
-  ) as any;
-
-  if (!result || result.length === 0) {
-    console.error("Tenant logo not found for tenant ID:", tenent_id);
-    return;
-  }
-
-  let tenantLogo = result[0].logo;
-  let name;
-  if (!result[0].logo) {
-    const result = await sequelize.query(
-      `SELECT name FROM tenant WHERE id = :tenent_id`,
-      {
-        replacements: { tenent_id },
-        type: QueryTypes.SELECT
-      }) as any;
-    if (!result || result.length === 0) {
-      console.error("Tenant name not found for tenant ID:", tenent_id);
-      return;
-    }
-    name = result[0].name;
-  }
-
-  const user = await sequelize.query(
-    `SELECT * FROM user WHERE program_id =:program_id AND id =:user_id`,
-    {
-      replacements: { program_id: payload.program_id, user_id: payload.userId },
-      type: QueryTypes.SELECT
-    }
-  )
-  const userData = user[0] as any
-  if (!userData) {
-    console.error("User not found for program ID:", payload.program_id, "and user ID:", payload.userId);
-    return;
-  }
-
-  const notificationUrl = getNotificationUrl();
-
-  if (!notificationUrl) {
-    console.error('Notification URL is not defined in environment variables.');
-    return;
-  }
-
-  if (!validateToken(payload.token)) {
-    return;
-  }
-
-  const fullName = `${userData.first_name} ${userData.middle_name} ${userData.last_name}`;
-  const created_by_first_name = userData.first_name;
-  const created_by_last_name = userData.last_name;
-
-  const data = payload.payload;
-  Object.assign(data, {
-    fullName,
-    created_by_first_name,
-    created_by_last_name,
-    tenantLogo,
-    name,
-  });
-
-  const notificationData = {
-    entityRefId: "simplifyvms",
-    program_id: payload.program_id,
-    traceId: payload.traceId,
-    eventCode: payload.eventCode,
-    channels: [
-      "EMAIL"
-    ],
-    recipient: {
-      email: {
-        to: [
-          {
-            email: payload.recipientEmail
-          }
-        ],
-        sender: {
-          email: "noreply@simplifyvms.com"
+    const programData = await sequelize.query(
+        `SELECT * FROM programs WHERE id =:program_id`,
+        {
+            replacements: { program_id: payload.program_id },
+            type: QueryTypes.SELECT
         }
-      }
-    },
-    payload: data,
-    userId: payload.userId ?? "",
-    language: "en"
-  };
+    )
+    const program = programData[0] as any
+    if (!program) {
+        console.error("Program not found for ID:", payload.program_id);
+        return;
+    }
+    const tenent_id = program.client_id || program.msp_id;
+    if (!tenent_id) {
+        console.error("Tenant ID is missing in the payload.");
+        return;
+    }
 
-  const updatedData = {
-    ...data,
-    fullName,
-    created_by_first_name,
-    created_by_last_name
-  };
+    const result = await sequelize.query(
+        `SELECT logo FROM tenant WHERE id = :tenent_id`,
+        {
+            replacements: { tenent_id },
+            type: QueryTypes.SELECT
+        }
+    ) as any;
 
-console.log(notificationData.recipient.email);
+    if (!result || result.length === 0) {
+        console.error("Tenant logo not found for tenant ID:", tenent_id);
+        return;
+    }
 
-  try {
-    const response = await axios.post(
-      `${notificationUrl}/notification-message/`,
-      
-        notificationData,
-      
-      {
-        headers: {
-          'Content-Type': 'application/json',
-          Authorization: `Bearer ${payload.token}`,
+    let tenantLogo = result[0].logo;
+    let name = result[0].name;
+
+    const user = await sequelize.query(
+        `SELECT * FROM user WHERE program_id =:program_id AND id =:user_id`,
+        {
+            replacements: { program_id: payload.program_id, user_id: payload.userId },
+            type: QueryTypes.SELECT
+        }
+    )
+    const userData = user[0] as any
+    if (!userData) {
+        console.error("User not found for program ID:", payload.program_id, "and user ID:", payload.userId);
+        return;
+    }
+
+    const notificationUrl = getNotificationUrl();
+
+    if (!notificationUrl) {
+        console.error('Notification URL is not defined in environment variables.');
+        return;
+    }
+
+    if (!validateToken(payload.token)) {
+        return;
+    }
+
+    const fullName = `${userData.first_name} ${userData.middle_name || ""} ${userData.last_name}`;
+    const created_by_first_name = userData.first_name;
+    const created_by_last_name = userData.last_name;
+
+    const data = payload.payload;
+    Object.assign(data, {
+        fullName,
+        created_by_first_name,
+        created_by_last_name,
+        logo_url: tenantLogo,
+        name: name
+    });
+
+    const notificationData = {
+        entityRefId: "simplifyvms",
+        program_id: payload.program_id,
+        traceId: payload.traceId,
+        eventCode: payload.eventCode,
+        channels: [
+            "EMAIL"
+        ],
+        recipient: {
+            email: {
+                to: [
+                    {
+                        email: payload.recipientEmail
+                    }
+                ],
+                sender: {
+                    email: "noreply@simplifyvms.com"
+                }
+            }
         },
-      }
-    );
+        payload: data,
+        userId: payload.userId ?? "",
+        language: "en"
+    };
 
-console.log(response);
+    try {
+        const response = await axios.post(
+            `${notificationUrl}/notification-message/`, notificationData,
+            {
+                headers: {
+                    'Content-Type': 'application/json',
+                    Authorization: `Bearer ${payload.token}`,
+                },
+            }
+        );
 
-  } catch (error: any) {
-    console.error('Failed to send notification:', error);
-  }
+    } catch (error: any) {
+        console.error('Failed to send notification:', error);
+    }
 }
