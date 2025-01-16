8f6384eb36e6960551d6b4b9b20e8925f4c80821
Vaibhav Nehe <90456257+vaibhavnehe70@users.noreply.github.com>
2025-01-16 18:26:28 +0530

    Refactor permission validation and enhance error handling in routes

--- src/routes/program-config.routes.ts
+++ src/routes/program-config.routes.ts
@@ -8,53 +8,65 @@ import {
   getTransformedConfig,
   getConfigByProgramIdAndTitles
 } from "../controllers/program-config.controller";
-import { Permissions,Actions } from "../constants/permissions";
+import { Permissions, Actions } from "../constants/permissions";
 import { validatePermissions } from "../middlewares/vaildate-permissions";
 
 async function programsConfigRoutes(fastify: FastifyInstance) {
-  fastify.get("/program/:program_id/program-config/:id",{
-    preHandler: validatePermissions,
-    config: {
-      permissions: [Permissions.PROGRAM_CONFIGURATION],
-      action: Actions.READ,
-    },
-  }, getConfigurationById);
-  fastify.get("/program/:program_id/configurations",{
-    preHandler: validatePermissions,
-    config: {
-      permissions: [Permissions.PROGRAM_CONFIGURATION],
-      action: Actions.READ,
-    },
-  }, getConfigByProgramIdAndTitles);
+  fastify.get("/program/:program_id/program-config/:id",
+    // {
+    //   preHandler: validatePermissions,
+    //   config: {
+    //     permissions: [Permissions.PROGRAM_CONFIGURATION],
+    //     action: Actions.READ,
+    //   },
+    // },
+    getConfigurationById);
+  fastify.get("/program/:program_id/configurations",
+    // {
+    //   preHandler: validatePermissions,
+    //   config: {
+    //     permissions: [Permissions.PROGRAM_CONFIGURATION],
+    //     action: Actions.READ,
+    //   },
+    // },
+    getConfigByProgramIdAndTitles);
   fastify.post("/program-config", createConfiguration);
-  fastify.put("/program/:program_id/program-config",{
-    preHandler: validatePermissions,
-    config: {
-      permissions: [Permissions.PROGRAM_CONFIGURATION],
-      action: Actions.UPDATE,
+  fastify.put("/program/:program_id/program-config",
+    // {
+    //   preHandler: validatePermissions,
+    //   config: {
+    //     permissions: [Permissions.PROGRAM_CONFIGURATION],
+    //     action: Actions.UPDATE,
+    //   },
+    // },
+    updateConfiguration);
+  fastify.delete("/program/:program_id/program-config/:id",
+    {
+      preHandler: validatePermissions,
+      config: {
+        permissions: [Permissions.PROGRAM_CONFIGURATION],
+        action: Actions.DELETE,
+      },
     },
-  }, updateConfiguration);
-  fastify.delete("/program/:program_id/program-config/:id",{
-    preHandler: validatePermissions,
-    config: {
-      permissions: [Permissions.PROGRAM_CONFIGURATION],
-      action: Actions.DELETE,
-    },
-  }, deleteConfiguration);
-  fastify.get("/program/:program_id/program-config",{
-    preHandler: validatePermissions,
-    config: {
-      permissions: [Permissions.PROGRAM_CONFIGURATION],
-      action: Actions.READ,
-    },
-  }, getProgramConfigurations);
-  fastify.get("/program/:program_id/program-configuration",{
-    preHandler: validatePermissions,
-    config: {
-      permissions: [Permissions.PROGRAM_CONFIGURATION],
-      action: Actions.READ,
-    },
-  }, getTransformedConfig);
+    deleteConfiguration);
+  fastify.get("/program/:program_id/program-config",
+    // {
+    //   preHandler: validatePermissions,
+    //   config: {
+    //     permissions: [Permissions.PROGRAM_CONFIGURATION],
+    //     action: Actions.READ,
+    //   },
+    // },
+    getProgramConfigurations);
+  fastify.get("/program/:program_id/program-configuration",
+    // {
+    //   preHandler: validatePermissions,
+    //   config: {
+    //     permissions: [Permissions.PROGRAM_CONFIGURATION],
+    //     action: Actions.READ,
+    //   },
+    // },
+    getTransformedConfig);
 }
 
 export default programsConfigRoutes;
