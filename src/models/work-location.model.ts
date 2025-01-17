9d682266eb3c8e60843c2402542bdbcf87eedf0f
kandalkarpriyanka <kandalkarpriyanka99@gmail.com>
2025-01-16 22:44:20 -0800

    disgurd push file

--- src/models/work-location.model.ts
+++ src/models/work-location.model.ts
@@ -1,4 +1,3 @@
-
 import { DataTypes, Model } from "sequelize";
 import { sequelize } from "../config/instance";
 import { Programs } from "./programs.model";
@@ -13,7 +12,7 @@ class WorkLocationModel extends Model {
   countries: any;
   states: any;
   state_name: any;
-   currency_id: any;
+  currency_id: any;
 }
 
 WorkLocationModel.init(
