334270c003bcf4ad9cc73f11ad2bca283fddd56a
Gauri Khemnar <gaurikhemnar@gmail.com>
2025-01-18 00:11:38 +0530

    Work on rate and hierarchie

--- src/utility/queries.ts
+++ src/utility/queries.ts
@@ -483,13 +483,16 @@ WITH hierarchy_cte AS (
     h.created_on, -- Include created_on
     h.program_id,
     h.is_deleted,
-    ph.name AS parent_hierarchy_name -- Fetch parent hierarchy name
+    dh.name AS default_hierarchy_name,
+    ph.name AS parent_hierarchy_name 
   FROM hierarchies h
   LEFT JOIN hierarchies ph
-    ON h.parent_hierarchy_id = ph.id -- Self-join to get parent name
+    ON h.parent_hierarchy_id = ph.id 
+  LEFT JOIN hierarchies dh
+    ON h.default_hierarchy_id = dh.id
   WHERE h.program_id = :program_id
     AND h.is_deleted = false
-    ${hasName ? 'AND h.name LIKE :name' : ''} -- Conditionally apply name filter
+    ${hasName ? 'AND h.name LIKE :name' : ''} 
     ${hasIsEnabled ? 'AND h.is_enabled = :is_enabled' : ''}
     ${startDate !== undefined && endDate !== undefined
     ? 'AND h.modified_on BETWEEN :startDate AND :endDate'
@@ -499,7 +502,6 @@ WITH hierarchy_cte AS (
 total_count_cte AS (
   SELECT COUNT(*) AS total_count FROM hierarchy_cte
 )
-
 SELECT
   h.*,
   (SELECT total_count FROM total_count_cte) AS total_count
@@ -513,9 +515,6 @@ ORDER BY
   h.id
 LIMIT :limit OFFSET :offset;
 `;
-
-
-
 
 // export const vendorDataQuery = `
 // SELECT
