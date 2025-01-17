00df31f6091d8dcdba75dcc636e9e7c0d26b6590
Gauri Khemnar <gaurikhemnar@gmail.com>
2025-01-17 10:54:38 +0530

    Get vendor markup based on hierarchie rate model

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,6 +1,6 @@
 import { FastifyRequest, FastifyReply } from "fastify";
 import User from "../models/user.model";
-import { UserFoundationalData, UserInterface } from "../interfaces/user.interface";
+import { UserInterface } from "../interfaces/user.interface";
 import generateCustomUUID from "../utility/genrateTraceId";
 import { baseSearch } from "../utility/baseService";
 import hierarchies from "../models/hierarchies.model";
