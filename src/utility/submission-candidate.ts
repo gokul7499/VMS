59615e49ae6998be93f7b7c41ce85a473354e3fb
ashishsshinde <74764695+ashishsshinde@users.noreply.github.com>
2025-01-22 15:08:58 +0530

    added curl

--- src/utility/submission-candidate.ts
+++ src/utility/submission-candidate.ts
@@ -8,7 +8,7 @@ export async function fetchUnavailableCandidates(
 ): Promise<number[]> {
     try {
         const response = await axios.get(
-            `https://v4-devnlb.simplifysandbox.net:8002/sourcing/v1/api/program/${program_id}/submission-candidate`,
+            `https://v4-qa.simplifysandbox.net/sourcing/v1/api/program/${program_id}/submission-candidate`,
             {
                 params: { job_id },
                 headers: { Authorization: `Bearer ${token}` }
