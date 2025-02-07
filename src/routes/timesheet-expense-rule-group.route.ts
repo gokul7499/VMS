ac9a4a5e5bbc93e69dacbd362aaa280c0553dafb
@pratikshakarad <karadpratiksha36@gmail.com>
2025-02-07 17:09:33 +0530

    add schema validation

--- src/routes/timesheet-expense-rule-group.route.ts
+++ src/routes/timesheet-expense-rule-group.route.ts
@@ -6,11 +6,35 @@ import {
     getAllTimesheetExpenseRuleGroups,
     getTimesheetExpenseRuleGroupById
 } from '../controllers/timesheet-expense-rule-group.controller';
+import { createTimesheetExpenseRuleGroupSchema, paramsSchema, querySchema } from '../interfaces/timesheet-expense-rule-group.interface';
 
 export default async function timesheetExpenseRuleGroupRoutes(fastify: FastifyInstance) {
-    fastify.post('/timesheet-expense-rule-groups', createTimesheetExpenseRuleGroup);
-    fastify.get('/timesheet-expense-rule-groups/get-all', getAllTimesheetExpenseRuleGroups);
-    fastify.get('/timesheet-expense-rule-groups/:id', getTimesheetExpenseRuleGroupById);
-    fastify.put('/timesheet-expense-rule-groups/:id', updateTimesheetExpenseRuleGroup);
-    fastify.delete('/timesheet-expense-rule-groups/:id', deleteTimesheetExpenseRuleGroup);
+    fastify.post('/timesheet-expense-rule-groups',{
+        schema: {
+            params: paramsSchema,
+           body:createTimesheetExpenseRuleGroupSchema
+        }
+    }, createTimesheetExpenseRuleGroup);
+    fastify.get('/timesheet-expense-rule-groups/get-all', {
+        schema: {
+            params: paramsSchema,
+            querystring:querySchema
+        }
+    },getAllTimesheetExpenseRuleGroups);
+    fastify.get('/timesheet-expense-rule-groups/:id',{
+        schema: {
+            params: paramsSchema,
+        }
+    }, getTimesheetExpenseRuleGroupById);
+    fastify.put('/timesheet-expense-rule-groups/:id',{
+        schema: {
+            params: paramsSchema,
+           body:createTimesheetExpenseRuleGroupSchema
+        }
+    }, updateTimesheetExpenseRuleGroup);
+    fastify.delete('/timesheet-expense-rule-groups/:id', {
+        schema: {
+            params: paramsSchema
+        }
+    },deleteTimesheetExpenseRuleGroup);
 }
