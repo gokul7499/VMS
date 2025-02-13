76adafeb5e1af976ef23b983654389338291c889
Siddhi <siddhiwaman29@gmail.com>
2025-02-13 16:11:57 +0530

    schema-validation

--- src/routes/custom-fields.routes.ts
+++ src/routes/custom-fields.routes.ts
@@ -8,13 +8,41 @@ import {
   searchCustomFields,
   updateCustomFieldsIsdisable
 } from '../controllers/custom-fields.controller';
+import { createCustomFieldsSchema, paramsSchema } from '../interfaces/custom-fields.interface';
 
 async function customFieldsRoutes(fastify: FastifyInstance) {
-  fastify.post('/custom-fields', saveCustomFields);
-  fastify.get('/program/:program_id/custom-fields', getAllCustomFields);
-  fastify.get('/program/:program_id/custom-fields/:id', getCustomFieldById);
-  fastify.put('/program/:program_id/custom-fields/:id', updateCustomFieldById);
-  fastify.delete('/program/:program_id/custom-fields/:id', deleteCustomField);
+  fastify.post('/custom-fields',{
+    schema: {
+      body:createCustomFieldsSchema,
+    }
+  },
+     saveCustomFields);
+  fastify.get('/program/:program_id/custom-fields',
+    {
+      schema:{
+       
+         params: paramsSchema,
+      }
+    }, getAllCustomFields);
+  fastify.get('/program/:program_id/custom-fields/:id',{
+    schema:{
+       params: paramsSchema,
+    }
+  }, getCustomFieldById);
+  fastify.put('/program/:program_id/custom-fields/:id',{
+    schema:{
+      body:createCustomFieldsSchema,
+      params: paramsSchema,
+
+    }
+  }, updateCustomFieldById);
+  fastify.delete('/program/:program_id/custom-fields/:id',
+    {
+      schema:{
+        params:paramsSchema
+      }
+    },
+     deleteCustomField);
   fastify.get('/program/:program_id/custom-fields/search', searchCustomFields);
   fastify.put('/program/:program_id/custom-fields/:id/enable-disable', updateCustomFieldsIsdisable); 
 }
