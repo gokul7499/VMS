7f04a1cf365298e0ae7884077071103bcff08561
rutujadighe9 <rutujadighe9@gmail.com>
2025-02-14 14:50:55 +0530

    add validation foundation-data

--- src/interfaces/foundational-data.interface.ts
+++ src/interfaces/foundational-data.interface.ts
@@ -16,3 +16,47 @@ export interface FoundationalDataInterface {
     depended_fields: any,
     is_billable: boolean
 }
+
+export const paramsSchema = {
+    type: 'object',
+    properties: {
+        program_id: { type: 'string' },
+        id: { type: 'string' }
+    },
+    required: ['program_id']
+};
+export const querySchema = {
+    type: 'object',
+    properties: {
+        search: { type: 'string' },
+        limit: { type: 'integer' },
+        offset: { type: 'integer' }
+    }
+};
+export const createFoundationalDataSchema = {
+    type: 'object',
+    required: ['name'],
+    properties: {
+        name: { type: 'string' },
+        description: { type: 'string' },
+        is_enabled: { type: 'boolean' },
+        associations: { type: 'array' },
+        program_id: { type: 'string' },
+        configuration: {
+            type: 'object',
+            properties: {
+                user_association_exclude: { type: 'boolean' },
+                allow_multiple_default_values: { type: 'boolean' },
+                view_only: { type: 'boolean' },
+                financial_master_data_type: { type: 'boolean' },
+                require_owner: { type: 'boolean' },
+                track_owner: { type: 'boolean' },
+                timesheet_master_data: { type: 'boolean' },
+                allow_multiple_jobs: { type: 'boolean' },
+                allow_multiple_sows: { type: 'boolean' },
+                contigent_option: { type: 'string' },
+                service_option: { type: 'string' },
+            },
+        }
+    }
+}
