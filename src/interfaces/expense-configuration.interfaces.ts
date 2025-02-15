4fc43ce02c20cf4bd594434b5ec662bb1e52d834
@pratikshakarad <karadpratiksha36@gmail.com>
2025-02-15 17:18:32 +0530

    update model

--- src/interfaces/expense-configuration.interfaces.ts
+++ src/interfaces/expense-configuration.interfaces.ts
@@ -1,23 +1,22 @@
-
 export interface ExpenseConfigurationAttributes {
   id: string;
-  config_name?: string;
-  status?: boolean;
+  name?: string;
   program_id: string;
-  week_end_day?: string;
-  thresholds?: any;
-  remove_msp_access_general?: any;
-  remove_user_access_misc?: any;
-  revoke_user_access?: any;
+  enable_thresholds?: boolean;
+  weekending_day?: string;
+  mdt_display_headers?: any;
+  misc_exp_access_rules?: any;
+  general_exp_access_rules?: any;
+  revoke_worker_access?: any;
+  general_exp_incurred_submission?: any;
   project?: any;
+  master_data?: any;
   is_enabled: boolean;
   is_deleted: boolean;
   created_on: number;
-  modified_on: number; 
+  modified_on: number;
   created_by?: string;
   updated_by?: string;
   hierarchy?: any; 
   expense_item_type_config?: any; 
-  master_data?:any
 }
-  
