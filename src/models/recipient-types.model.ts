5f56f52191ca7b67726624d11ee04c4262331240
ishwari <gunjalishwari27@gmail.com>
2025-02-26 16:13:05 +0530

    commit changes

--- src/models/recipient-types.model.ts
+++ src/models/recipient-types.model.ts
@@ -70,12 +70,10 @@ RecipientType.init(
         created_on: {
             type: DataTypes.DOUBLE,
             allowNull: true,
-            defaultValue: DataTypes.NOW
         },
         updated_on: {
             type: DataTypes.DOUBLE,
             allowNull: true,
-            defaultValue: DataTypes.NOW
         },
         created_by: {
             type: DataTypes.UUID,
