b701922a19bf8ce6cd38aefa7710bbbaed3805d4
Gauri Khemnar <gaurikhemnar@gmail.com>
2025-02-27 16:28:27 +0530

    Update hooks

--- src/hooks/timeFormatHook.ts
+++ src/hooks/timeFormatHook.ts
@@ -3,8 +3,8 @@ import { Model } from 'sequelize';
 export const beforeSave = (record: Model) => {
     if (record.isNewRecord) {
         record.set('created_on', Date.now());
-        record.set('modified_on', Date.now());
+        record.set('updated_on', Date.now());
     } else {
-        record.set('modified_on', Date.now());
+        record.set('updated_on', Date.now());
     }
 };
