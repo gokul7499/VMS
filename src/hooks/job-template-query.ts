9777fef7287bd10371546aaa0dc6a2ce99379206
kandalkarpriyanka <kandalkarpriyanka99@gmail.com>
2025-03-06 01:44:28 -0800

    add is_enabled condition to get recent and popular job-templet

--- src/hooks/job-template-query.ts
+++ src/hooks/job-template-query.ts
@@ -47,7 +47,8 @@ class JobTempletRepository {
     hierarchy_ids: string[],
     job_type?: string,
     limit?: number,
-    offset?: number
+    offset?: number,
+    is_enabled?:boolean
   ) {
     const hierarchyCondition = hierarchy_ids.length > 0
       ? `AND job_template_hierarchies.hierarchy IN (${hierarchy_ids.map(() => '?').join(',')})`
@@ -56,6 +57,7 @@ class JobTempletRepository {
     const paginationCondition = limit !== undefined && offset !== undefined
       ? `LIMIT ? OFFSET ?`
       : '';
+      const isEnabledCondition = is_enabled !== undefined ? `AND job_templates.is_enabled = ?` : '';
 
     const query = `
       SELECT
@@ -79,6 +81,7 @@ class JobTempletRepository {
       WHERE job_templates.program_id = ?
       ${hierarchyCondition}
       ${jobTypeCondition}
+      ${isEnabledCondition}
       AND job_templates.job_submitted_count >=1
       GROUP BY job_templates.template_name
       ORDER BY job_submitted_count DESC
@@ -92,6 +95,10 @@ class JobTempletRepository {
       replacements.push(limit, offset);
     }
 
+    if (is_enabled !== undefined) {
+      replacements.push(is_enabled ? 1 : 0); 
+    }
+
     const data = await sequelize.query(query, {
       replacements,
       type: QueryTypes.SELECT,
@@ -100,13 +107,14 @@ class JobTempletRepository {
     return data;
   }
 
-  async getJobTempletByHierarchies(program_id: string, hierarchy_ids: string[], job_type?: string) {
+  async getJobTempletByHierarchies(program_id: string, hierarchy_ids: string[], job_type?: string,is_enabled?: boolean|undefined) {
     let hierarchyCondition = '';
 
     if (hierarchy_ids.length > 0) {
       hierarchyCondition = `AND job_template_hierarchies.hierarchy IN (${hierarchy_ids.map(() => '?').join(',')})`;
     }
     const jobTypeCondition = job_type ? `AND job_templates.job_type = ?` : '';
+    const isEnabledCondition = is_enabled !== undefined ? `AND job_templates.is_enabled = ?` : '';
     const query = `
       SELECT
         job_templates.template_name,
@@ -129,12 +137,16 @@ class JobTempletRepository {
       WHERE job_templates.program_id = ?
       ${hierarchyCondition}
       ${jobTypeCondition}
+      ${isEnabledCondition}
       GROUP BY job_templates.template_name
       ORDER BY created_on DESC;
     `;
-    const replacements = [program_id, ...hierarchy_ids];
+    const replacements: (string | number)[] = [program_id, ...hierarchy_ids];
     if (job_type) {
       replacements.push(job_type);
+    }
+    if (is_enabled !== undefined) {
+      replacements.push(is_enabled ? 1 : 0); 
     }
 
     const data = await sequelize.query(query, {
