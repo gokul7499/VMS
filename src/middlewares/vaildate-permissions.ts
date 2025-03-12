7bf6206f5792be542dc81957d88ecf9e9a3119ea
Vaibhav Nehe <90456257+vaibhavnehe70@users.noreply.github.com>
2025-03-12 22:57:18 +0530

    refactor: improve response structure and enhance permission validation logic

--- src/middlewares/vaildate-permissions.ts
+++ src/middlewares/vaildate-permissions.ts
@@ -2,46 +2,41 @@ import { FastifyRequest, FastifyReply, HookHandlerDoneFunction } from 'fastify';
 import { checkPermission } from '../utility/check-permissions';
 import generateCustomUUID from '../utility/genrateTraceId';
 
-export function validatePermissions(request: FastifyRequest<{ Params: { program_id: string } }>, reply: FastifyReply, done: HookHandlerDoneFunction): void {
-  const config = request.routeOptions?.config || {};
-  const { permissions = [], action = '' } = config as {
-    permissions?: string[];
-    action?: string;
-  };
+export function validatePermissions(action: string, permissions: string[]) {
+  return function (request: FastifyRequest<{ Params: { program_id: string } }>, reply: FastifyReply, done: HookHandlerDoneFunction): void {
+    const token = request.headers.authorization;
+    const { program_id } = request.params;
 
-  const token = request.headers.authorization;
-
-  const { program_id } = request.params;
-
-  if (!token) {
-    reply.status(401).send({
-      status_code: 401,
-      message: "Unauthorized: Missing authorization token",
-      trace_id: generateCustomUUID(),
-    });
-    return done();
-  }
-
-  if (!program_id) {
-    reply.status(401).send({
-      status_code: 401,
-      message: "Unauthorized: Missing program ID",
-      trace_id: generateCustomUUID(),
-    });
-    return done();
-  }
-
-  console.log('Validating permissions', permissions, action);
-
-  checkPermission(token, program_id, { permissions }, action)
-    .then(() => done())
-    .catch((error) => {
-      console.error(error);
+    if (!token) {
       reply.status(401).send({
         status_code: 401,
-        message: error.message,
+        message: "Unauthorized: Missing authorization token",
         trace_id: generateCustomUUID(),
       });
-      done(error);
-    });
+      return done();
+    }
+
+    if (!program_id) {
+      reply.status(401).send({
+        status_code: 401,
+        message: "Unauthorized: Missing program ID",
+        trace_id: generateCustomUUID(),
+      });
+      return done();
+    }
+
+    console.log('Validating permissions', permissions, action);
+
+    checkPermission(token, program_id, { permissions }, action)
+      .then(() => done())
+      .catch((error) => {
+        console.error(error);
+        reply.status(401).send({
+          status_code: 401,
+          message: error.message,
+          trace_id: generateCustomUUID(),
+        });
+        done(error);
+      });
+  };
 }
