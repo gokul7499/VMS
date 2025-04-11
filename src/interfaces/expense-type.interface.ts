b5c47c7f8c92c55ee291065212386180a38247f4
ruturaj <ruturajgunjal764@gmail.com>
2025-04-11 11:47:35 +0530

    refactor code

--- src/interfaces/expense-type.interface.ts
+++ src/interfaces/expense-type.interface.ts
@@ -1,62 +1,61 @@
 export interface ExpenseTypeInterface {
-    id: string;
-    name?: string;
-    category?: string;
-    code?: string;
-    is_enabled: boolean;
-    is_attachment_mandatory: boolean;
-    is_notes_mandatory: boolean;
-    apply_msp_fee: boolean;
-    apply_tax: boolean;
-    allow_negative_expense: boolean;
-    unit_based: boolean;
-    unit_label?: string;
-    rate_per_unit?: number;
-    max_unit_limit?: number;
     program_id: string;
-    created_on?: bigint;
-    updated_on?: bigint;
-    created_by?: string;
-    updated_by?: string;
+    id: string;
+    name: string;
+    code: string;
+    category: 'general' | 'miscellaneous';
+    is_attachments_mandatory: boolean;
+    is_notes_mandatory: boolean;
+    is_msp_fees_applied: boolean;
+    is_tax_applied: boolean;
+    is_negative_expense_allowed: boolean;
+    is_unit_based: boolean;
+    unit_label: string | null;
+    rate_per_unit: number | null;
+    max_unit_limit: number | null;
+    is_enabled: boolean;
     is_deleted: boolean;
-}
-export const paramsSchema = {
+    created_on: number;
+    modified_on: number;
+    created_by: string;
+    modified_by: string;
+  }
+  
+  export const paramsSchema = {
     type: 'object',
     properties: {
-        program_id: { type: 'string' },
-        id: { type: 'string' }
+      program_id: { type: 'string', format: 'uuid' },
+      id: { type: 'string', format: 'uuid' }
     },
     required: ['program_id']
-};
-export const querySchema = {
+  };
+  
+  export const querySchema = {
     type: 'object',
     properties: {
-        search: { type: 'string' },
-        limit: { type: 'integer' },
-        offset: { type: 'integer' }
+      search: { type: 'string' },
+      limit: { type: 'integer' },
+      offset: { type: 'integer' }
     }
-};
-
-export const createExpenseTypeSchema = {
+  };
+  
+  export const createExpenseTypeSchema = {
     type: 'object',
     properties: {
-        name: { type: 'string' },
-        category: { type: 'string' },
-        code: { type: 'string' },
-        is_enabled: { type: 'boolean' },
-        unit_based: {
-            type: 'object',
-            properties: {
-                unitType: { type: 'string' },
-                rate: { type: 'string' },
-                max_limit: { type: 'string' }
-            }
-        },
-        is_mandatory_attachment: { type: 'boolean' },
-        is_manadatory_notes: { type: 'boolean' },
-        apply_msp_fee: { type: 'boolean' },
-        allow_unit_based: { type: 'boolean' },
-        allow_negative_expense: { type: 'boolean' },
-        appply_tax: { type: 'boolean' }
-    }
-};
+      name: { type: 'string' },
+      category: { type: 'string', enum: ['general', 'miscellaneous'] },
+      code: { type: 'string' },
+      is_enabled: { type: 'boolean' },
+      is_attachments_mandatory: { type: 'boolean' },
+      is_notes_mandatory: { type: 'boolean' },
+      is_msp_fees_applied: { type: 'boolean' },
+      is_tax_applied: { type: 'boolean' },
+      is_negative_expense_allowed: { type: 'boolean' },
+      is_unit_based: { type: 'boolean' },
+      unit_label: { type: 'string' },
+      rate_per_unit: { type: 'number' },
+      max_unit_limit: { type: 'number' }
+    },
+    
+  };
+  
--- src/models/expense-type.model.ts
+++ src/models/expense-type.model.ts
@@ -5,131 +5,128 @@ import { beforeSave } from '../hooks/timeFormatHook';
 import { Programs } from "./programs.model";
 
 class ExpenseTypeModel extends Model {
-    id: any;
-    name: any;
-    code: any;
-    program_id: any;
-    appply_tax: any;
-    is_negative_expense_allow: any;
+  id: any;
+  name: any;
+  code: any;
+  program_id: any;
+  is_tax_applied: any;
+  is_negative_expense_allowed: any;
 }
 
 ExpenseTypeModel.init({
-    id: {
-        type: DataTypes.UUID,
-        defaultValue: DataTypes.UUIDV4,
-        allowNull: false,
-        primaryKey: true,
+  id: {
+    type: DataTypes.UUID,
+    defaultValue: DataTypes.UUIDV4,
+    allowNull: false,
+    primaryKey: true,
+  },
+  program_id: {
+    type: DataTypes.UUID,
+    allowNull: false,
+    references: {
+      model: Programs,
+      key: "id"
+    }
+  },
+  name: {
+    type: DataTypes.STRING,
+    allowNull: true
+  },
+  category: {
+    type: DataTypes.ENUM("general", "miscellaneous"),
+    allowNull: true
+  },
+  code: {
+    type: DataTypes.STRING,
+    allowNull: true
+  },
+  is_enabled: {
+    type: DataTypes.BOOLEAN,
+    defaultValue: true,
+  },
+  is_attachments_mandatory: {
+    type: DataTypes.BOOLEAN,
+    defaultValue: true,
+  },
+  is_notes_mandatory: {
+    type: DataTypes.BOOLEAN,
+    defaultValue: true,
+  },
+  is_msp_fees_applied: {
+    type: DataTypes.BOOLEAN,
+    defaultValue: true,
+  },
+  is_tax_applied: {
+    type: DataTypes.BOOLEAN,
+    defaultValue: false
+  },
+  is_negative_expense_allowed: {
+    type: DataTypes.BOOLEAN,
+    defaultValue: false
+  },
+  is_unit_based: {
+    type: DataTypes.BOOLEAN,
+    defaultValue: false
+  },
+  unit_label: {
+    type: DataTypes.STRING,
+    allowNull: true
+  },
+  rate_per_unit: {
+    type: FLOAT,
+    allowNull: true
+  },
+  max_unit_limit: {
+    type: FLOAT,
+    allowNull: true
+  },
+  is_deleted: {
+    type: DataTypes.BOOLEAN,
+    defaultValue: false,
+  },
+  created_on: {
+    type: DataTypes.BIGINT.UNSIGNED,
+    defaultValue: Date.now(),
+    allowNull: true,
+  },
+  updated_on: {
+    type: DataTypes.BIGINT.UNSIGNED,
+    defaultValue: Date.now(),
+    allowNull: true
+  },
+  created_by: {
+    type: DataTypes.UUID,
+    allowNull: true,
+  },
+  updated_by: {
+    type: DataTypes.UUID,
+    allowNull: true,
+  },
+}, {
+  sequelize,
+  tableName: 'expense_item_type_config',
+  timestamps: false,
+  hooks: {
+    beforeSave: (instance) => {
+      beforeSave(instance);
     },
-    name: {
-        type: DataTypes.STRING,
-        allowNull: true
+    beforeValidate: async (instance) => {
+      convertEmptyStringsToNull(instance);
+      if (!instance.code && instance.program_id) {
+        const program = await Programs.findByPk(instance.program_id);
+        if (program?.name) {
+          const programPrefix = program.name.substring(0, 3).toUpperCase();
+          const count = await ExpenseTypeModel.count();
+          const sequence = (count + 1).toString().padStart(5, '0');
+          instance.code = `${programPrefix}-EXP-${sequence}`;
+        }
+      }
     },
-    category: {
-        type: DataTypes.STRING,
-        allowNull: true
-    },
-    code: {
-        type: DataTypes.STRING,
-        allowNull: true
-    },
-    is_enabled: {
-        type: DataTypes.BOOLEAN,
-        defaultValue: true,
-    },
-    is_attachment_mandatory: {
-        type: DataTypes.BOOLEAN,
-        defaultValue: true,
-    },
-    is_notes_mandatory: {
-        type: DataTypes.BOOLEAN,
-        defaultValue: true,
-    },
-    apply_msp_fee: {
-        type: DataTypes.BOOLEAN,
-        defaultValue: true,
-    },
-    appply_tax: {
-        type: DataTypes.BOOLEAN,
-        defaultValue: false
-    },
-    is_negative_expense_allow: {
-        type: DataTypes.BOOLEAN,
-        defaultValue: false
-    },
-    unit_based: {
-        type: DataTypes.BOOLEAN,
-        defaultValue: false
-    },
-    unit_label:{
-        type:DataTypes.STRING,
-        allowNull:true
-    },
-    program_id:{
-        type: DataTypes.UUID,
-        allowNull: false,
-        references:{
-           model:Programs,
-            key:"id"
-        } 
-    },
-    rate_per_unit :{
-        type:FLOAT,
-        allowNull:true
-    },
-    max_unit_limit :{
-        type:FLOAT,
-        allowNull:true
-    },
-   
-    is_deleted: {
-        type: DataTypes.BOOLEAN,
-        defaultValue: false,
-    },
-    created_on: {
-        type: DataTypes.BIGINT.UNSIGNED,
-        defaultValue: Date.now(),
-        allowNull: true,
-    },
-    updated_on: {
-        type: DataTypes.BIGINT.UNSIGNED,
-        defaultValue: Date.now(),
-        allowNull: true
-    },
-    created_by: {
-        type: DataTypes.UUID,
-        allowNull: true,
-    },
-    updated_by: {
-        type: DataTypes.UUID,
-        allowNull: true,
-    },
-},
-    {
-        sequelize,
-        tableName: 'expense_item_type_config',
-        timestamps: false,
-        hooks: {
-            beforeSave: (instance) => {
-                beforeSave(instance);
-            },
-            beforeValidate: async (instance) => {
-                convertEmptyStringsToNull(instance);
-                if (!instance.code && instance.program_id) {
-                    const program = await Programs.findByPk(instance.program_id);
-                    if (program?.name) {
-                        const programPrefix = program.name.substring(0, 3).toUpperCase();
-                        const count = await ExpenseTypeModel.count();
-                        const sequence = (count + 1).toString().padStart(5, '0');
-                        instance.code = `${programPrefix}-EXP-${sequence}`;
-                    }
-                }
-            },
-        },
-    });
+  },
+});
 
 sequelize.sync();
 
 ExpenseTypeModel.belongsTo(Programs, { foreignKey: 'program_id' });
-
 
 export default ExpenseTypeModel;
