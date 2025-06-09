5ac811dbba616ad19d03d807ca538f7bddb118dc
Prasad Kale <prasad.kale@simplifyvms.com>
2025-06-09 16:29:00 +0530

    created filter and list api

--- src/routes/credentialing-packet.routes.ts
+++ src/routes/credentialing-packet.routes.ts
@@ -4,6 +4,8 @@ import {
     getCredentialingPacketById,
     updateCredentialingPacket,
     deleteCredentialingPacket,
+    filterCredentialingPacket,
+    listCredentialingPacket,
 } from "../controllers/credentialing-packet.controller";
 
 async function credentialingPacketRoutes(fastify: FastifyInstance) {
@@ -11,6 +13,9 @@ async function credentialingPacketRoutes(fastify: FastifyInstance) {
     fastify.get("/program/:program_id/credentialing-packets/entity/:entity_id", getCredentialingPacketById);
     fastify.put("/program/:program_id/credentialing-packets/entity/:entity_id", updateCredentialingPacket);
     fastify.delete("/program/:program_id/credentialing-packets/entity/:entity_id", deleteCredentialingPacket);
+    fastify.get("/program/:program_id/credentialing-packets/filter", filterCredentialingPacket);
+    fastify.get("/program/:program_id/credentialing-packets/list/brief", listCredentialingPacket);
+
 }
 
 export default credentialingPacketRoutes;
