86c42bf63a315449e1db2f3e8a38ab38993dbe57
Gauri Khemnar <gaurikhemnar@gmail.com>
2025-06-10 12:41:27 +0530

    Change route

--- src/routes/timesheet-type-config.route.ts
+++ src/routes/timesheet-type-config.route.ts
@@ -59,7 +59,7 @@ async function timesheetTypeConfigRoutes(fastify: FastifyInstance) {
         // preHandler: validatePermissions(Actions.READ, [Permissions.TIMESHEET_TYPE_CONFIGURATION])
     }, TimesheetTypeConfigController.timesheetTypeConfigFilter);
 
-    fastify.post('/timeshet-type/get-all', {
+    fastify.post('/timeshet-type', {
         schema: {
             params: paramsSchema,
             body: timesheetTypeConfigFilterSchema
