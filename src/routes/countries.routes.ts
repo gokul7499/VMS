6d2bdb5776e3ab7e32bc00770acf7661fb2adff4
sakshi patole <sakshipatole75@gmail.com>
2025-06-16 18:16:52 +0530

    remove authorization

--- src/routes/countries.routes.ts
+++ src/routes/countries.routes.ts
@@ -7,8 +7,10 @@ import {
   updateCountry,
   deleteCountry
 } from '../controllers/countries.controllers';
+import { verifyToken } from '../middlewares/verifyToken';
 
 export default async function countriesRoutes(fastify: FastifyInstance) {
+  fastify.addHook('preHandler', verifyToken);
   fastify.post('/countries/',createCountry);
   fastify.post('/countries/bulk-upload',bulkUploadCountry);
   fastify.get('/countries/get-all', getCountries)
