23b5e4a5fe176318fe8b48acb2106be9a363c22e
Sakshi Raut <sakshiraut568@gmail.com>
2025-06-17 10:15:19 +0530

    commit changes

--- src/routes/dist-schedule-detail.routes.ts
+++ src/routes/dist-schedule-detail.routes.ts
@@ -1,6 +1,8 @@
 import { FastifyInstance } from 'fastify'
 import { createDistScheduleDetails, deleteDistScheduleDetail, getAllDistScheduleDetails, getDistScheduleDetailById, updateDistScheduleDetail } from '../controllers/dist-schedule-detail.controller';
+import { verifyToken } from '../middlewares/verifyToken';
 async function distScheduleRoutes(fastify: FastifyInstance) {
+    fastify.addHook('preHandler', verifyToken);
     fastify.get('/program/:program_id/dist-schedules', getAllDistScheduleDetails);
     fastify.get('/program/:program_id/dist-schedules/:id', getDistScheduleDetailById);
     fastify.post('/program/:program_id/dist-schedules',createDistScheduleDetails );
